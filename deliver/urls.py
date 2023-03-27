4a3befd33e8a2ac8a2ba10ee74aa71815466d7f8
CedricLittman <cedric@littman.me.uk>
2023-03-27 13:56:56 +0000

    Added category item models

--- deliver/urls.py
+++ deliver/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from customer.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
