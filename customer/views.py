697d0e162902df28da317d346f8febd14bd5c095
CedricLittman <cedric@littman.me.uk>
2023-03-27 11:59:38 +0000

    Part 1 - Header and Navbar and footer

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/index.html')
+
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/about.html')
--- deliver/urls.py
+++ deliver/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from customer.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
 ]
