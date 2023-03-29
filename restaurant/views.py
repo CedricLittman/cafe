34e3b5baa664f10acb7140891631e2f0ecb1e9d9
CedricLittman <cedric@littman.me.uk>
2023-03-29 16:43:30 +0000

    Working on dashboard

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.utils.timezone import datetime
 from customer.models import OrderModel
 
@@ -8,11 +9,11 @@ from customer.models import OrderModel
 class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
     def get(self, request, *args, **kwargs):
 
-        # get the current date
+        # add date
         today = datetime.today()
         orders = OrderModel.objects.filter(created_on__year=today.year, created_on__month=today.month, created_on__day=today.day)
 
-        # loop through the orders and add the price value, check if order is not shipped
+        # calculate total order value & check if sent
         unshipped_orders = []
         total_revenue = 0
         for order in orders:
@@ -21,7 +22,7 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
             if not order.is_shipped:
                 unshipped_orders.append(order)
 
-        # pass total number of orders and total revenue into template
+        # arrange orders for display in template
         context = {
             'orders': unshipped_orders,
             'total_revenue': total_revenue,
