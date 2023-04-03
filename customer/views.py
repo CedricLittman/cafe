9676eb7bf9aaba887e72b7b875d2c463d89acd88
CedricLittman <cedric@littman.me.uk>
2023-04-03 14:56:50 +0000

    Working on HTML

--- customer/views.py
+++ customer/views.py
@@ -51,7 +51,7 @@ class Order(View):
         items = request.POST.getlist('items[]')
 
         for item in items:
-            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            menu_item = MenuItem.objects.get(pk__exact=int(item))
             item_data = {
                 'id': menu_item.pk,
                 'name': menu_item.name,
